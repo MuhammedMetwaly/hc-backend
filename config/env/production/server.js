973bcfc709dccda2f2e205e1dfe1600ca52e646e
muhammedmetwaly94 <midodevi94@gmail.com>
2023-01-11 01:04:51 +0200

    prepare for render deploy

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,3 @@
- // path: ./config/env/production/server.js
-
- module.exports = ({ env }) => ({
-    proxy: true,
-    url: env('APP_URL'), // replaces `host` and `port` properties in the development environment
-    app: { 
-      keys: env.array('APP_KEYS')
-    },
+module.exports = ({ env }) => ({
+  url: env("RENDER_EXTERNAL_URL"),
 });
