8d6469658869b287b99f35d99ffc920d08234aab
muhammedmetwaly94 <midodevi94@gmail.com>
2023-01-14 19:32:30 +0200

    changing some configs in db

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,17 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(
-    env("DATABASE_URL")
-  );
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
   return {
     connection: {
       client: "postgres",
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
+        host: env("DATABASE_HOST"),
+        port: env.init("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: env.bool('DATABASE_SSL', true)
       },
       debug: false,
       pool: {
