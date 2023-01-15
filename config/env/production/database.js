92357311b7689b120542a018c289406dde0ca111
muhammedmetwaly94 <midodevi94@gmail.com>
2023-01-15 02:10:41 +0200

    change server url

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,17 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  // const { host, port, database, user, password } = parse(env("DATABASE_URL"));
   return {
     connection: {
       client: "postgres",
       connection: {
         host: env("DATABASE_HOST"),
-        port: env("DATABASE_PORT"),
+        port: env.int("DATABASE_PORT"),
         database: env("DATABASE_NAME"),
         user: env("DATABASE_USERNAME"),
         password: env("DATABASE_PASSWORD"),
-        ssl: env.bool('DATABASE_SSL', true)
+        ssl: env.bool("DATABASE_SSL", true),
       },
       debug: false,
       pool: {
